8e4bc964ae84197c8e3085f5a2ab25f86edb0650
crazyrabbitpei <you@example.com>
2016-09-08 17:34:13 +0800

    TODO:sharedposts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 var track_tool = require('./tool/track_tool.js');
 var router = require('./router.js');
+var EventEmitter = require('events');
 var fs = require('graceful-fs');
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -8,25 +9,144 @@ var app = express();
 var http = require('http');
 var server = http.createServer(app);
 
+
 var setting = JSON.parse(fs.readFileSync('./service/setting.json'));
 var serverip = setting['serverip'];
 var serverport = setting['serverport'];
 var crawler_name = setting['crawler_name'];
 var crawler_version = setting['crawler_version'];
-var mission_token = setting['mission_token'];
+
+var master_ip = setting['master_ip'];
+var master_port = setting['master_port'];
+var master_name = setting['master_name'];
+var master_version = setting['master_version'];
+var master_timeout_again = setting['master_timeout_again'];
+var invite_token = setting['invite_token'];
+var request_timeout = setting['request_timeout'];
+
 var mode = setting['mode'];
-
-var mission_token;
 var track_posts;
-var graph_request_interval;
-var graph_timeout_again;
-var site;
-var graph_version;
-var graph_token;
-var fields;
 
-var apply_status;
+var all_fetch=3;//有兩個資訊有下一頁問題：reactions, comments，要等到這兩個都抓完後程式才算完成
+var graph_request=0;//計算總共發了多少Graph api;
 var trackids=[];
+var current_post_id='';//目前正在抓取的id
+var final_result;//存放所有結果
+
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
+myEmitter.on('nextcomment', (link) => {
+    graph_request++;
+    track_tool.fetchNextPage(request_timeout,mode,link,function(flag,msg){
+        if(flag=='err'){
+            console.log('[fetchNextPage err] '+msg);
+        }
+        else{
+            track_tool.parseComment(msg);
+            if(final_result.next_comments!=null){
+                myEmitter.emit('nextcomment',final_result.next_comments);
+            }
+            else{
+                all_fetch--;
+                if(all_fetch==0){
+                    myEmitter.emit('one_post_done');
+                }
+            }
+        }
+    });
+});
+myEmitter.on('nextsharedpost', (link) => {
+    graph_request++;
+    track_tool.fetchNextPage(request_timeout,mode,link,function(flag,msg){
+        if(flag=='err'){
+            console.log('[fetchNextPage err] '+msg);
+        }
+        else{
+            track_tool.parseSharedpost(msg);
+            if(final_result.next_sharedposts!=null){
+                myEmitter.emit('nextcomment',final_result.next_sharedposts);
+            }
+            else{
+                all_fetch--;
+                if(all_fetch==0){
+                    myEmitter.emit('one_post_done');
+                }
+            }
+        }
+    });
+});
+myEmitter.on('nextreaction', (link) => {
+    graph_request++;
+    track_tool.fetchNextPage(request_timeout,mode,link,function(flag,msg){
+        if(flag=='err'){
+            console.log('[fetchNextPage err] '+msg);
+        }
+        else{
+            track_tool.parseReaction(msg);
+            var i;
+            var reac_type = Object.keys(final_result.reactions);
+            //console.log('link:'+JSON.stringify(final_result.next_reactions));
+            if(final_result.next_reactions!=null){
+                myEmitter.emit('nextreaction',final_result.next_reactions);
+            }
+            else{
+                all_fetch--;
+                if(all_fetch==0){
+                    myEmitter.emit('one_post_done');
+                }
+            }
+        }
+    });
+});
+myEmitter.on('one_post_done', () => {
+    var i,cnt=0;
+    var reac_type = Object.keys(final_result.reactions);
+    console.log('======['+current_post_id+']======')
+    console.log('--All reactions--');
+    for(i=0;i<reac_type.length;i++){
+        console.log('['+reac_type[i]+'] '+final_result.reactions[reac_type[i]]);
+        cnt+=final_result.reactions[reac_type[i]];
+    }
+    final_result.reactions_cnt = cnt;
+    console.log('final_result.reactions_cnt:'+final_result.reactions_cnt);
+
+    console.log('--All comments--');
+    final_result.comments_cnt = final_result.comments.length;
+    console.log('final_result.comments_cnt:'+final_result.comments_cnt);
+
+    console.log('--All sharedposts--');
+    final_result.sharedposts_cnt = final_result.sharedposts.length;
+    console.log('final_result.sharedposts_cnt:'+final_result.sharedposts_cnt);
+    /*
+       for(i=0;i<final_result.comments.length;i++){
+       console.log('['+i+'] '+final_result.comments[i].message);
+       }
+    */
+
+   console.log('--Total graph request ['+graph_request+'] --');
+   if(mode['status']=='test'){
+       track_tool.writeRec('json',current_post_id,JSON.stringify(final_result,null,3));
+   }
+   /*Reset基本記錄*/
+   all_fetch=2;
+   graph_request=0;
+   final_result=null;
+   /*開始搜集下一個*/
+   var temp = trackids.shift();
+   if(typeof temp!=='undefined'){
+       current_post_id = temp['id'];
+       start(current_post_id);
+   }
+   else{
+        console.log('All post id have been tracked.');
+        /*向track master要下一批post id*/
+        if(mode['status']!='test'){
+            
+        }
+   }
+
+
+});
 /**
  * --階段--
  * 測試模式(test)，為單機執行 不需要開啟api，先測試是否可以正確搜集到資料 並將資料合併成gaisRec，基本參數設定在loca端的setting.json file裡
@@ -35,7 +155,7 @@ var trackids=[];
  *      - next page
  *      - error handler
  *      - retry handler
- *  - result formater
+ *  - final_result formater
  *  - track_posts array manager
  *  - next page manager
  * 正式模式(formal)，需要監聽master傳送的資料，包含可搜集的track_posts和mission內容，並能將資料正確回傳至master，且告知當前任務已完成
@@ -48,94 +168,90 @@ var trackids=[];
  * 
 **/
 if(mode['status']=='test'){
-    mission_token='test';
+    invite_token='test';
     track_posts = mode['track_posts'];
-    graph_request_interval = mode['graph_request_interval'];
-    graph_timeout_again = mode['graph_timeout_again'];
-    site = mode['site'];
-    graph_version = mode['graph_version'];
-    graph_token = mode['graph_token'];
-    fields = mode['fields'];
 
-    var post_id = track_posts.shift()['id'];
-    //var promise = new Promise(function(resolve,reject){
-        track_tool.trackPost(setting['request_timeout'],mode,post_id,(flag,msg)=>{
-            if(flag=='err'){
-                console.log('[trackPost err] '+msg);
-            }
-            else{
-                //console.log(JSON.stringify(msg,null,3));
-                var result = track_tool.initContent(msg);
-                track_tool.fetchNextPage(setting['request_timeout'],result.next_comments,function(flag,msg){
-                    if(flag=='err'){
-                        console.log('[fetchNextPage err] '+msg);
-                    }
-                    else{
-                        track_tool.parseComment(result,msg,function(comments){
-                            //console.log('All comments:\n'+comments);
-                        });
-                    }
-                });
-
-                track_tool.fetchNextPage(setting['request_timeout'],result.next_reactions,function(flag,msg){
-                    if(flag=='err'){
-                        console.log('[fetchNextPage err] '+msg);
-                    }
-                    else{
-                        track_tool.parseReaction(result,msg,function(reactions){
-                            console.log('All reactions:'+reactions);
-                        });
-                    }
-                });
-                //resolve(result);
-                /*
-                 * 下一頁存放位置: 
-                 *   comments next link : this.next_comments
-                 *   reactions next link : this.next_reactions
-                 *   comments' likes next link : this.comments[i]['likes_next']
-                 *
-                 * 數值還會在變動:(因為下一頁)
-                 *   reactions['LIKE'] reaction['HAHA']...
-                 *   comments[i]['likes']
-                 * */
-                //console.log(JSON.stringify(result,null,3));
-
-                //track_tool.fetchNextPage(setting['request_timeout'],result.next_comments);
-            }
-        //});
-    });
-    /*
-    promise.then(function(result){
-        console.log('First page:'+JSON.stringify(result,null,3));
-        track_tool.fetchNextPage(setting['request_timeout'],result.next_comments,function(flag,msg){
-            if(flag=='err'){
-                console.log('[fetchNextPage err] '+msg);
-            }
-            else{
-                track_tool.parseComment(result,msg,function(comments){
-                    console.log('Second page:'+JSON.stringify(comments,null,3));
-                });
-            }
-        });
-    }).catch(function(error){
-        console.log('[promise err] '+error);
-    })
-    */
+    trackids = track_posts;
+    var temp = trackids.shift();
+    if(typeof temp!=='undefined'){
+        current_post_id = temp['id'];
+        start(current_post_id);
+    }
 }
 else{
-    apply_status=0;
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true  }));
     app.use('/'+crawler_name+'/'+crawler_version,router);
     //程式啟動，向tracking master報告
     server.listen(serverport,serverip,function(){
-        console.log("[Server start] ["+new Date()+"] http work at "+serverip+":"+serverport);
-        //TODO
-        /*啟動後，向master報告並等待master指派任務，在master認證成功前 mission api都會鎖住*/
-        /*
-           applyCrawler(serverport,serverip,mission_token,(err,msg){
-           });
-           */
+            console.log("[Server start] ["+new Date()+"] http work at "+serverip+":"+serverport);
+            //TODO
+            /*啟動後，向master報告並等待master指派任務，在master認證成功前 mission api都會鎖住*/
+            applyCrawler(master_ip,master_port,master_name,master_version,invite_token,request_timeout,master_timeout_again,(err,mission)=>{
+                track_posts = mission['track_posts'];
+                trackids = track_posts;
+                var temp = trackids.shift();
+                if(typeof temp!=='undefined'){
+                    current_post_id = temp['id'];
+                    start(current_post_id);
+                }
+            });
     })
 }
 
+function start(track_id){
+    graph_request++;
+    track_tool.trackPost(request_timeout,mode,track_id,(flag,msg)=>{
+        if(flag=='err'){
+            console.log('[trackPost err] '+msg);
+        }
+        else{
+            final_result = track_tool.initContent(msg);
+
+            /*搜集下一頁的comments*/
+            if(final_result.next_comments!=null){
+                myEmitter.emit('nextcomment',final_result.next_comments);                   
+            }
+            else{
+                all_fetch--;
+                if(all_fetch==0){
+                    myEmitter.emit('one_post_done');
+                }
+            }
+            /*搜集下一頁的sharedposts*/
+            if(final_result.next_sharedposts!=null){
+                myEmitter.emit('nextsharedpost',final_result.next_sharedposts);                   
+            }
+            else{
+                all_fetch--;
+                if(all_fetch==0){
+                    myEmitter.emit('one_post_done');
+                }
+            }
+            /*搜集下一頁reactions*/
+            var i;
+            var reac_type = Object.keys(final_result.reactions);
+
+            //console.log('link:'+JSON.stringify(final_result.next_reactions));
+            if(final_result.next_reactions!=null){
+                myEmitter.emit('nextreaction',final_result.next_reactions);     
+            }
+            else{
+                all_fetch--;
+                if(all_fetch==0){
+                    myEmitter.emit('one_post_done');
+                }
+            }
+            /*
+             * 下一頁存放位置: 
+             *   comments next link : this.next_comments
+             *   reactions next link : this.next_reactions
+             *   comments' likes next link : this.comments[i]['likes_next']
+             *
+             * 數值還會在變動:(因為下一頁)
+             *   reactions['LIKE'] reaction['HAHA']...
+             *   comments[i]['likes']
+             * */
+        }
+    });
+}
--- tool/track_tool.js
+++ tool/track_tool.js
@@ -6,7 +6,7 @@ var HashMap = require('hashmap');
 var reactions;
 var setting = JSON.parse(fs.readFileSync('./service/setting.json'));
 
-function applyCrawler(ip,port,master_name,master_version,token,timeout,fin){
+function applyCrawler(ip,port,master_name,master_version,token,timeout,retryt,fin){
     request({
         method:'POST',
         json:true,
@@ -17,14 +17,57 @@ function applyCrawler(ip,port,master_name,master_version,token,timeout,fin){
             invite_token:token
         },
         url:'http://'+ip+':'+port+'/'+master_name+'/'+master_version,
-        timeout:timeout
+        timeout:timeout*1000
     },(err,res,body)=>{
+        if(!err&&res.statusCode==200){
+            var err_msg='';
+            var err_flag=0
+            try{
+                var content = JSON.parse(body);
+            }
+            catch(e){
+                err_flag=1;
+                err_msg=e;
+            }
+            finally{
+                if(err_flag==1){
+                    fin('err',err_msg);
+                }
+                else{
+                    fin('ok',content);   
+                }
+            }
 
+        }
+        else{
+            if(err){
+                if(err.code.indexOf('TIME')!=-1||err.code.indexOf('ENOT')!=-1||err.code.indexOf('ECONN')!=-1||err.code.indexOf('REACH')!=-1){
+                    setTimeout(function(){
+                        applyCrawler(ip,port,master_name,master_version,token,timeout,retryt,fin)
+                    },retryt*1000);
+                }
+                else{
+                    writeLog('err','applyCrawler, '+err);
+                    fin('err','applyCrawler, '+err);
+                }
+            }
+            else{
+                if(res.statusCode>=500&&res.statusCode<600){
+                    setTimeout(function(){
+                        applyCrawler(ip,port,master_name,master_version,token,timeout,retryt,fin)
+                    },retryt*1000);
+                }
+                else{
+                    writeLog('err','applyCrawler, '+res['statusCode']+', '+res['body']);
+                    fin('err','applyCrawler, '+res['statusCode']+', '+res['body']);
+                }
+            }
+       }
     });
 }
 function trackPost(timeout,mission,post_id,fin){
-    var site = mission['site']+mission['graph_version']+'/'+post_id+'?fields='+mission['fields']+'&access_token='+mission['graph_token'];
-    console.log('Request:'+site);
+    var site = mission['site']+mission['graph_version']+'/'+post_id+'?fields='+mission['fields']+'&access_token='+mission['graph_token']+'&limit='+mission['limit'];
+    console.log('\nRequest:'+site);
     request({
         url:site,
         timeout:timeout*1000
@@ -54,7 +97,7 @@ function trackPost(timeout,mission,post_id,fin){
                 if(err.code.indexOf('TIME')!=-1||err.code.indexOf('ENOT')!=-1||err.code.indexOf('ECONN')!=-1||err.code.indexOf('REACH')!=-1){
                     setTimeout(function(){
                         trackPost(site,timeout);
-                    },retryt*1000);
+                    },mission['graph_timeout_again']*1000);
                 }
                 else{
                     writeLog('err','trackPost, '+err);
@@ -65,7 +108,7 @@ function trackPost(timeout,mission,post_id,fin){
                 if(res.statusCode>=500&&res.statusCode<600){
                     setTimeout(function(){
                         trackPost(site,timeout);
-                    },retryt*1000);
+                    },mission['graph_timeout_again']*1000);
                 }
                 else{
                     writeLog('err','trackPost, '+res['statusCode']+', '+res['body']);
@@ -76,13 +119,16 @@ function trackPost(timeout,mission,post_id,fin){
     });
 }
 function initContent(content){
-    this.id = content['id'];
-    this.created_time = content['created_time'];
+    this.groupid = content['from']['id'];
+    this.groupname = content['from']['name'];
+    this.postid = content['id'];
+    this.created_time = dateFormat(content['created_time'],'yyyy/mm/dd HH:MM:ss');
     this.permalink_url = content['permalink_url'];
     this.link = content['link'];
-    this.shares = content['shares'];
+    this.shares = content['shares']['count'];
     this.reactions = {};
     this.comments = [];
+    this.sharedposts = [];
 
     //TODO:需要測試沒有下一頁狀況的數值為和
     if(typeof content['reactions']==='undefined'){
@@ -110,35 +156,65 @@ function initContent(content){
 
     }
 
+    if(typeof content['sharedposts']==='undefined'){
+            this.next_sharedposts = null;
+    }
+    else{
+        if(content['sharedposts']['paging']['next']==='undefined'){
+            this.next_sharedposts = null;
+        }
+        else{
+            this.next_sharedposts = content['sharedposts']['paging']['next'];
+        }
+
+    }
 
     var type,cnt,i;
-    var cnt_reaction = new HashMap();
-
     //TODO:需要一次將所有reactions page拜訪完
     for(i=0;i<content['reactions']['data'].length;i++){
         type = content['reactions']['data'][i]['type'];
-        if(!cnt_reaction.has(type)){
-            cnt_reaction.set(type,0);
-            this.reactions[type]=0;
+        if(typeof this.reactions[type]==='undefined'){
+            this.reactions[type]=1;
         }
         else{
-           cnt = cnt_reaction.get(type);
-           cnt++;
-           cnt_reaction.set(type,cnt);
            this.reactions[type]++;
         }
     }
     
     var data,likes_next='',comments_likes;
-    //TODO:需要將所有的comments和comment的likes page都拜訪完
-    for(i=0;i<content['comments']['data'].length;i++){
-        data = content['comments']['data'];
-        if(typeof data[i]['likes']==='undefined'){
+    if(typeof content['comments']!=='undefined'){
+        //TODO:將來要將所有的comment的likes page都拜訪完，目前先記錄下一頁的連結和第一頁的likes數
+        for(i=0;i<content['comments']['data'].length;i++){
+            data = content['comments']['data'];
+            if(typeof data[i]['likes']==='undefined'){
                 likes_next=null;
                 comments_likes=0;
+            }
+            else{
+                comments_likes=data[i]['likes']['data'].length;
+                if(typeof data[i]['likes']['paging']['next']==='undefined'){
+                    likes_next=null;
+                }
+                else{
+                    likes_next=data[i]['likes']['paging']['next'];
+                }
+            }
+            this.comments.push({id:data[i].id,created_time:dateFormat(data[i].created_time,'yyyy/mm/dd HH:MM:ss'),likes:comments_likes,likes_next:likes_next,message:data[i].message});
+
+        }
+    }
+
+
+    var data,likes_next='',sharedposts_likes;
+    //TODO:將來要將所有的sharedposts的likes page都拜訪完，目前先記錄下一頁的連結和第一頁的likes數
+    for(i=0;i<content['sharedposts']['data'].length;i++){
+        data = content['sharedposts']['data'];
+        if(typeof data[i]['likes']==='undefined'){
+            likes_next=null;
+            sharedposts_likes=0;
         }
         else{
-            comments_likes=data[i]['likes']['data'].length;
+            sharedposts_likes=data[i]['likes']['data'].length;
             if(typeof data[i]['likes']['paging']['next']==='undefined'){
                 likes_next=null;
             }
@@ -146,49 +222,32 @@ function initContent(content){
                 likes_next=data[i]['likes']['paging']['next'];
             }
         }
-        this.comments.push({id:data[i].id,created_time:data[i].created_time,likes:comments_likes,likes_next:likes_next,message:data[i].message});
+        this.sharedposts.push({id:data[i].id,created_time:dateFormat(data[i].created_time,'yyyy/mm/dd HH:MM:ss'),likes:sharedposts_likes,likes_next:likes_next,message:data[i].message});
 
     }
-    /*
-    if(this.next_comments!=null){
-        fetchNextPage(setting['request_timeout'],this.next_comments,function(flag,msg){
-            if(flag=='err'){
-                console.log('[err] '+msg);
-            }
-            else{
-                //console.log('Next page:'+JSON.stringify(msg,null,3));
-                parseComment(msg,function(result){
-                    console.log('Next page:'+JSON.stringify(result,null,3));
-                });
-            }
-        });
-    }
-    if(this.next_reactions!=null){
-        
-    }
-    */
-
     return this;
 }
-
-function parseComment(current_data,content,fin){
+function parseComment(content){
     if(typeof content['data']==='undefined'){
-            current_data.next_comments = null;
+            this.next_comments = null;
     }
     else{
         if(content['paging']['next']==='undefined'){
-            current_data.next_comments = null;
+            this.next_comments = null;
         }
         else{
-            current_data.next_comments = content['paging']['next'];
+            this.next_comments = content['paging']['next'];
         }
 
     }
-    
     var data,likes_next='',comments_likes,i;
     //TODO:需要將所有的comments和comment的likes page都拜訪完
     for(i=0;i<content['data'].length;i++){
         data = content['data'];
+        if(typeof data[i]['message']==='undefined'){
+            continue;
+        }
+
         if(typeof data[i]['likes']==='undefined'){
                 likes_next=null;
                 comments_likes=0;
@@ -202,67 +261,75 @@ function parseComment(current_data,content,fin){
                 likes_next=data[i]['likes']['paging']['next'];
             }
         }
-        current_data.comments.push({id:data[i].id,created_time:data[i].created_time,likes:comments_likes,likes_next:likes_next,message:data[i].message});
+        this.comments.push({id:data[i].id,created_time:dateFormat(data[i].created_time,'yyyy/mm/dd HH:MM:ss'),likes:comments_likes,likes_next:likes_next,message:data[i].message});
     }
-    
-    if(current_data.next_comments!=null){
-        fetchNextPage(setting['request_timeout'],current_data.next_comments,function(flag,msg){
-            if(flag=='err'){
-                console.log('[err] '+msg);
-            }
-            else{
-                parseComment(current_data,msg,fin);
-            }
-        });
+    return this;
+}
+function parseSharedpost(content){
+    if(typeof content['data']==='undefined'){
+            this.next_sharedposts = null;
     }
     else{
-        fin(JSON.stringify(current_data,null,3));
+        if(content['paging']['next']==='undefined'){
+            this.next_sharedposts = null;
+        }
+        else{
+            this.next_sharedposts = content['paging']['next'];
+        }
+
     }
+    var data,likes_next='',sharedposts_likes,i;
+    for(i=0;i<content['data'].length;i++){
+        data = content['data'];
+        if(typeof data[i]['message']==='undefined'){
+            continue;
+        }
+
+        if(typeof data[i]['likes']==='undefined'){
+                likes_next=null;
+                sharedposts_likes=0;
+        }
+        else{
+            sharedposts_likes=data[i]['likes']['data'].length;
+            if(typeof data[i]['likes']['paging']['next']==='undefined'){
+                likes_next=null;
+            }
+            else{
+                likes_next=data[i]['likes']['paging']['next'];
+            }
+        }
+        this.sharedposts.push({id:data[i].id,created_time:dateFormat(data[i].created_time,'yyyy/mm/dd HH:MM:ss'),likes:sharedposts_likes,likes_next:likes_next,message:data[i].message});
+    }
+    return this;
 }
-function parseReaction(current_data,content,fin){
-    console.log(JSON.stringify(current_data.reactions,null,3))
+function parseReaction(content){
     //TODO:需要測試沒有下一頁狀況的數值為和
     if(typeof content['data']==='undefined'){
-        current_data.next_reactions = null;
+        this.next_reactions = null;
     }
     else{
         if(typeof content['paging']['next']==='undefined'){
-            current_data.next_reactions = null;
+            this.next_reactions = null;
         }
         else{
-            current_data.next_reactions = content['paging']['next'];
+            this.next_reactions = content['paging']['next'];
         }
     }
 
     var type,cnt,i;
-    var cnt_reaction = new HashMap();
-
-    //TODO:需要一次將所有reactions page拜訪完
     for(i=0;i<content['data'].length;i++){
         type = content['data'][i]['type'];
-        if(typeof current_data.reactions[type]==='undefined'){
-            current_data.reactions[type]=0;
+        if(typeof this.reactions[type]==='undefined'){
+            this.reactions[type]=1;
         }
-        current_data.reactions[type]++;
+        else{
+            this.reactions[type]++;
+        }
+
     }
-    fin(JSON.stringify(current_data,null,3));
-    /*
-    if(current_data.next_reactions!=null){
-        fetchNextPage(setting['request_timeout'],current_data.next_reactions,function(flag,msg){
-            if(flag=='err'){
-                console.log('[err] '+msg);
-            }
-            else{
-                parseReaction(current_data,msg,fin);
-            }
-        });
-    }
-    else{
-        fin(JSON.stringify(current_data,null,3));
-    }
-    */
+    return this;
 }
-function fetchNextPage(timeout,site,fin){
+function fetchNextPage(timeout,mission,site,fin){
     request({
         url:site,
         timeout:timeout*1000
@@ -292,7 +359,7 @@ function fetchNextPage(timeout,site,fin){
                 if(err.code.indexOf('TIME')!=-1||err.code.indexOf('ENOT')!=-1||err.code.indexOf('ECONN')!=-1||err.code.indexOf('REACH')!=-1){
                     setTimeout(function(){
                         fetchNextPage(site,timeout);
-                    },retryt*1000);
+                    },mission['graph_timeout_again']*1000);
                 }
                 else{
                     writeLog('err','fetchNextPage, '+err);
@@ -303,7 +370,7 @@ function fetchNextPage(timeout,site,fin){
                 if(res.statusCode>=500&&res.statusCode<600){
                     setTimeout(function(){
                         fetchNextPage(site,timeout);
-                    },retryt*1000);
+                    },mission['graph_timeout_again']*1000);
                 }
                 else{
                     writeLog('err','fetchNextPage, '+res['statusCode']+', '+res['body']);
@@ -351,10 +418,29 @@ function writeLog(type,msg){
     });
 }
 
+function writeRec(type,track_id,msg){
+    var now = new Date();
+    var date = dateFormat(now,'yyyymmdd');
+    var filename=setting['rec_dir']+'/';
+    if(type=='gais'){
+        filename += track_id+'.'+date+setting['gaisrec_filename'];
+    }
+    else{
+        filename += track_id+'.'+date+setting['jsonrec_filename'];
+    }
+    fs.writeFile(filename,msg,(err)=>{
+        if(err){
+            console.log('[writeLog] Error:'+err);
+        }
+    });
+}
 exports.sendResponse=sendResponse;
+exports.writeLog=writeLog;
+exports.writeRec=writeRec;
 exports.trackPost=trackPost;
 exports.mission=mission;
 exports.initContent=initContent;
 exports.fetchNextPage=fetchNextPage;
 exports.parseComment=parseComment;
+exports.parseSharedpost=parseSharedpost;
 exports.parseReaction=parseReaction;
